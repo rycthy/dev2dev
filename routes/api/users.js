47007167723f3eb9d05d2e6642a2e7d48cfe06ce
cephscope <sworm.null@gmail.com>
2018-10-11 19:38:50 -0400

    setup JSON web token payload upon successful login

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -57,21 +59,34 @@ router.post('/login', (req, res) => {
   User.findOne({ email })
     .then((user) => {
       // check for user
-      if(!user) {
+      if (!user) {
         return res.status(404).json({ email: 'User not found' })
       }
 
       // Check password
       bcrypt.compare(password, user.password)
-      .then((isMatch) => {
-        if (isMatch) {
-          res.json({ msg: 'Success' });
-        } else {
-          return res.status(400).json({ password: 'Password incorrect' });
-        }
-      })
+        .then((isMatch) => {
+          if (isMatch) {
+            // User Matched
+            const payload = { id: user.id, name: user.name, avatar: user.avatar } // create JWT payload
+
+            // Sign Token
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: `Bearer ${token}`
+                })
+              });
+          } else {
+            return res.status(400).json({ password: 'Password incorrect' });
+          }
+        })
     })
-  
+
 })
 
 module.exports = router;
