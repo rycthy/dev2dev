80eed7a82846e7209b8b7e2b04e532c787791c67
cephscope <sworm.null@gmail.com>
2018-10-13 19:01:15 -0400

    created experience input validation script

---
+++ validation/experience.js
@@ -0,0 +1,27 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+// TODO: standardize post-submission validation e.g. jon doe should be Jon Doe (if // that's the chosen standard)
+
+module.exports = function validateExperienceInput(data) {
+  let errors = {};
+
+  // TODO: refactor to be less verbose
+  data.title = !isEmpty(data.title) ? data.title : '';
+  data.company = !isEmpty(data.company) ? data.company : '';
+  data.from = !isEmpty(data.from) ? data.from : '';
+
+  if (Validator.isEmpty(data.title)) {
+    errors.title = "Please enter your title";
+  }
+  if (Validator.isEmpty(data.company)) {
+    errors.company = "Please enter your company";
+  }
+  if (Validator.isEmpty(data.from)) {
+    errors.from = "Please enter when you started this position";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
