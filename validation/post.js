7b1c509e0c93d7b53db8700de2765da7020856a2
cephscope <sworm.null@gmail.com>
2018-10-15 13:10:37 -0400

    created post input validation script

---
+++ validation/post.js
@@ -0,0 +1,22 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+// TODO: standardize post-submission validation e.g. jon doe should be Jon Doe (if // that's the chosen standard)
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  // TODO: refactor to be less verbose
+  data.text = !isEmpty(data.email) ? data.email : '';
+
+  if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = 'Post must be between 10 and 30 characters';
+  }
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Please enter your text";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
