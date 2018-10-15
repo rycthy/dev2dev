99e6a0ec56d057df2f0b1f50132f7c2e3863aafd
cephscope <sworm.null@gmail.com>
2018-10-15 13:23:25 -0400

    setup server-side post input validation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Post model
 const Post = require('../../models/Post');
+
+// Validation
+const validatePostInput = require('../../validation/post');
 
 // @route   GET api/posts/test
 // @desc    Tests posts route
@@ -18,8 +22,13 @@ router.get('/test',
 
 router.post('/', passport.authenticate('jwt', { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
     const newPost = new Post({
-      ...req.body
+      ...req.body,
+      user: req.user.id
     });
 
     newPost.save().then((post) => res.json(post))
