4b9d099604e4131611b9e03ec7ddba95121ccdd7
cephscope <sworm.null@gmail.com>
2018-10-15 14:37:12 -0400

    added 404 error when trying to get already-deleted posts, setup delete post by id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-// Post model
+// Import Models
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // Validation
 const validatePostInput = require('../../validation/post');
@@ -27,11 +28,15 @@ router.get('/', (req, res) => {
 });
 
 // @route   GET api/posts/:id
-// @desc    Get posts
+// @desc    Get post by ID
 // @access  Public
 router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
-    .then((post) => res.json(post))
+    .then((post) => {
+      if (!post) res.status(404).json({ nopostfound: 'Oops! Looks like that post is no more' })
+
+      return res.json(post);
+    })
     .catch((err) => res.status(404).json({ nopostfound: 'No post found with that id' }));
 });
 
@@ -52,6 +57,26 @@ router.post('/', passport.authenticate('jwt', { session: false }),
 
     newPost.save().then((post) => res.json(post))
       .catch((err) => res.status(400).json(err));
+  });
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post by post ID
+// @access  Private
+
+router.delete('/:id', passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then((post) => {
+        // Authorize post owner
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ notauthorized: 'User not authorized' })
+        }
+
+        return post.remove()
+          .then(() => res.json({ success: true }))
+          .catch((err) => res.status(400).json(err));
+      })
+      .catch((err) => res.status(404).json({ postnotfound: 'No post found' }));
   });
 
 module.exports = router;
