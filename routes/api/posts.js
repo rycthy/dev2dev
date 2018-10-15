4605ebd4d2207a0cdb3380eed48acf961a8dae12
cephscope <sworm.null@gmail.com>
2018-10-15 13:06:36 -0400

    setup create post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Post = require('../../models/Post');
 
 // @route   GET api/posts/test
 // @desc    Tests posts route
-// @access    Public
+// @access  Public
 
 router.get('/test',
   (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+
+router.post('/', passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      ...req.body
+    });
+
+    newPost.save().then((post) => res.json(post))
+      .catch((err) => res.status(400).json(err));
+  });
 
 module.exports = router;
