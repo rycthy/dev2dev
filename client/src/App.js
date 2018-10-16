663d4abdea7661a9e2e6bc75203bf2cc7f86e201
cephscope <sworm.null@gmail.com>
2018-10-16 15:11:44 -0400

    setup navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>dev2dev</h1>
       </div>
     );
