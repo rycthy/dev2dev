5293c5187cea2cf4c9e1c38d08c112d16a605389
cephscope <sworm.null@gmail.com>
2018-10-17 13:18:52 -0400

    setup persistent authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
 import configureStore from './configureStore';
+import { setCurrentUser } from './actions/authActions';
+import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -10,6 +13,13 @@ import Login from './components/auth/Login';
 import './App.css';
 
 const store = configureStore();
+
+// Check for token
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
