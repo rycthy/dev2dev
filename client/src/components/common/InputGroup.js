f6359912c8c12ab5eca0c57f8924d3c88982f890
cephscope <sworm.null@gmail.com>
2018-10-21 23:46:52 -0400

    fixed typo for input group class

--- client/src/components/common/InputGroup.js
+++ client/src/components/common/InputGroup.js
@@ -11,7 +11,7 @@ const InputGroup = ({
   icon
 }) => {
   return (
-    <div className="input-group mb3">
+    <div className="input-group mb-3">
       <div className="input-group-prepend">
         <span className="input-group-text">
           <i className={icon} />
