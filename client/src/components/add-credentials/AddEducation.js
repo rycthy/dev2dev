e07b4de64cfac0c1ba98348ae88b338a325f541e
cephscope <sworm.null@gmail.com>
2018-10-25 18:09:05 -0400

    created add-credentials component files

---
+++ client/src/components/add-credentials/AddEducation.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class AddEducation extends Component {
+  render() {
+    return;
+    <div>AddEducation</div>;
+  }
+}
+
+export default AddEducation;
---
+++ client/src/components/add-credentials/AddExperience.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class AddExperience extends Component {
+  render() {
+    return;
+    <div>AddExperience</div>;
+  }
+}
+
+export default AddExperience;
